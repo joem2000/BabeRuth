86b6384dca305c85ae8dd396b3d82132c2ddefe9
joem2000 <joemolanare@gmail.com>
2022-11-26 13:25:43 -0500

    Added files
    Have added these files and have already started

---
+++ script.js
@@ -0,0 +1,19 @@
+/*function funCoaches() {
+    window.open("coaches.html");
+}*/
+
+function funTeam() {
+
+}
+
+function funExpect() {
+
+}
+
+function funHome() {
+    history.back();
+}
+
+function funSign() {
+
+}
