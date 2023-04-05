6008f6e4a2a9d9d3d7e1fcb909a7d1597ec6f49e
joem2000 <joemolanare@gmail.com>
2023-04-05 00:16:42 -0400

    Some clean up and did some work to connect.php.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,15 +1,13 @@
 function signUp() {
-    let first = document.getElementById("first").value;
-    let last = document.getElementById("last").value;
+    
     let age = document.getElementById("ageBtn").value;
     let birthday = document.getElementById("DOB").value;
-    let parentFirst = document.getElementById("parent-first").value;
-    let parentLast = document.getElementById("parent-last").value; 
     let email = document.getElementById("email").value;
     let phoneNumber = document.getElementById("phone-number").value;
 
-    if (age < 12 || age > 16) {
-        alert("Player must be 12-16 years old!");
+    if (age < 13 || age > 16) {
+        alert("Player must be 13-16 years old!");
     }
+    
 
 }
